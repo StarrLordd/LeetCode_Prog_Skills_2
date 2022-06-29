87b9d4bbda3e707755af3c278aa6c76831d0477d
Amit Tripathi <amittripathi2233@gmai.com>
2022-06-29 12:12:44 +0530

    29/06/2022

---
+++ Day_3/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,27 @@
+import java.lang.String;
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<String> s=new Stack<>();
+        for(String str:tokens){
+            if(isOperator(str)){
+                int el2=Integer.parseInt(s.pop());
+                int el1=Integer.parseInt(s.pop());
+                int ans=0;
+                if(str.equals("+")) ans=el1+el2;
+                else if(str.equals("-")) ans=el1-el2;
+                else if(str.equals("*")) ans=el1*el2;
+                else if(str.equals("/")) ans=el1/el2;
+                s.push(String.valueOf(ans));
+            }
+            else{
+                s.push(str);
+            } 
+        }
+        return Integer.parseInt(s.peek());
+    }
+    boolean isOperator(String s){
+        if(s.equals("+")||s.equals("-")||s.equals("*")||s.equals("/"))
+            return true;
+        else return false;
+    }
+}
